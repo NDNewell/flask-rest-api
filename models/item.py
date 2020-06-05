1062ca5e5d4abc6b37e639a2d1c9740a85478682
Nicholas Newell <nicholas.newell@11fs.com>
2020-06-05 13:15:56 +0100

    fix: Rename cls -> ItemModel in ItemModel

--- models/item.py
+++ models/item.py
@@ -21,7 +21,7 @@ class ItemModel(db.Model):
 
     @classmethod
     def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first()
+        return ItemModel.query.filter_by(name=name).first()
 
     def save_to_db(self):
         db.session.add(self)
